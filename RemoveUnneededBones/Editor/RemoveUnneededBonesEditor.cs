c7827661876607c4c0b2e2a982ff38562223dfaa
ZettaiVR <zettaivr@gmail.com>
2023-08-14 21:46:06 +0200

    Add RemoveUnneededBones
    Removes unused bones from SkinnedMeshRenderers

---
+++ RemoveUnneededBones/Editor/RemoveUnneededBonesEditor.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System;
+using Zettai;
+#if UNITY_EDITOR
+using UnityEditor;
+#if VRC_SDK_VRCSDK2 || VRC_SDK_VRCSDK3
+using VRC.SDKBase.Editor.BuildPipeline;
+#endif
+//#if CVR_CCK_EXISTS
+//using ABI.CCK.Scripts.Editor;
+//#endif
+
+#if VRC_SDK_VRCSDK2 || VRC_SDK_VRCSDK3
+public class RemoveUnneededBones_VRC : IVRCSDKPreprocessAvatarCallback
+{
+    public int callbackOrder => 0;
+
+    public bool OnPreprocessAvatar(GameObject avatarGameObject)
+    {
+        try
+        {
+            RemoveUnneededBones.Remove(avatarGameObject.transform);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+        return true;
+    }
+}
+#endif
+/*
+ * Doesn't work due to CVR CCK not making a copy of the assets being uploaded, and saving them to scene after calling the Pre*BundleEvent 
+#if CVR_CCK_EXISTS
+[InitializeOnLoad]
+public class RemoveUnneededBones_CVR
+{
+    static RemoveUnneededBones_CVR()
+    {
+        CCK_BuildUtility.PreAvatarBundleEvent.AddListener(OnPreprocess);
+        CCK_BuildUtility.PrePropBundleEvent.AddListener(OnPreprocess);
+    }
+
+    public static void OnPreprocess(GameObject avatarGameObject)
+    {
+        try
+        {
+            //avatarGameObject = GameObject.Instantiate(avatarGameObject);
+            RemoveUnneededBones.Remove(avatarGameObject.transform);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+}
+#endif
+*/
+
+
+[CustomEditor(typeof(RemoveUnneededBones))]
+[CanEditMultipleObjects]
+[DisallowMultipleComponent]
+public class RemoveUnneededBones_Editor : Editor
+{
+    private const string EditorOnly = "EditorOnly";
+    public override void OnInspectorGUI()
+    {
+        RemoveUnneededBones script = (RemoveUnneededBones)target;
+        DrawDefaultInspector();
+        if (GUILayout.Button("Do it now"))
+        {
+            var previousTag = script.gameObject.tag;
+            var scriptName = script.GetType().Name;
+            if (previousTag == EditorOnly)
+            {
+                Debug.LogWarning($"[{scriptName}] Won't execute when the root is tagged with {EditorOnly} tag!", script.gameObject);
+                return;
+            }
+            script.gameObject.tag = scriptName;
+            try
+            {
+                RemoveUnneededBones.Remove(script.transform, script.save);
+            }
+            catch  (Exception ex) 
+            {
+                Debug.LogException(ex, script.gameObject);
+            }
+            script.gameObject.tag = previousTag;
+        }
+    }
+    public static void Clean(PlayModeStateChange state)
+    {
+        if (!state.HasFlag(PlayModeStateChange.ExitingEditMode))
+            return;
+        Debug.Log($"cleaned {RemoveUnneededBones.createdAssetNames.Count}");
+        foreach (var item in RemoveUnneededBones.createdAssetNames)
+        {
+            AssetDatabase.DeleteAsset(item);
+        }
+        var all = AssetDatabase.FindAssets("RemoveUnneededBonesTempAsset-*", new string[] { "Assets/temp" });
+        foreach (var item in all)
+        {
+            AssetDatabase.DeleteAsset(AssetDatabase.GUIDToAssetPath(item));
+        }
+    }
+    [InitializeOnLoad]
+    public static class PlayModeStateChangedExample
+    {
+        // register an event handler when the class is initialized
+        static PlayModeStateChangedExample()
+        {
+            AddTag(nameof(RemoveUnneededBones));
+            EditorApplication.playModeStateChanged += Clean;
+        }
+
+        private static void AddTag(string name)
+        {
+            var tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            var tagsProp = tagManager.FindProperty("tags");
+            if (PropertyExists(tagsProp, name))
+                return;
+            int index = tagsProp.arraySize;
+            tagsProp.InsertArrayElementAtIndex(index);
+            var sp = tagsProp.GetArrayElementAtIndex(index);
+            sp.stringValue = name;
+            tagManager.ApplyModifiedProperties();
+        }
+
+        private static bool PropertyExists(SerializedProperty property, string value)
+        {
+            if (property == null)
+                return true; // so we don't try to add to a null property
+            for (int i = 0; i < property.arraySize; i++)
+                if (property.GetArrayElementAtIndex(i)?.stringValue?.Equals(value) == true)
+                    return true;
+            return false;
+        }
+    }
+}
+#endif
---
+++ RemoveUnneededBones/RemoveUnneededBones.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Unity.Collections;
+using System.Reflection;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Zettai
+{
+    [RequireComponent(typeof(Animator))]
+    public class RemoveUnneededBones : MonoBehaviour
+    {
+        public bool save = true;
+        private static readonly List<SkinnedMeshRenderer> renderers = new List<SkinnedMeshRenderer>();
+        private static readonly Dictionary<int, int> indices = new Dictionary<int, int>();
+        private static readonly List<int> orderIndices = new List<int>();
+        private static readonly HashSet<int> usedIndices = new HashSet<int>();
+        private static readonly List<Matrix4x4> bindposes = new List<Matrix4x4>();
+        private static readonly List<Matrix4x4> newBindposes = new List<Matrix4x4>();
+        private static readonly List<Transform> newTransforms = new List<Transform>();
+        public static List<string> createdAssetNames = new List<string>();
+        private readonly static System.Diagnostics.Stopwatch sw1 = new System.Diagnostics.Stopwatch();
+        private readonly static System.Diagnostics.Stopwatch sw2 = new System.Diagnostics.Stopwatch();
+        public static void Remove(Transform root, bool save = true)
+        {
+            if (root == null || !root.CompareTag(MethodBase.GetCurrentMethod().DeclaringType.Name))
+                return;
+            sw1.Restart();
+            sw2.Reset();
+            root.GetComponentsInChildren(true, renderers);
+
+#if UNITY_EDITOR
+            if (!AssetDatabase.IsValidFolder("Assets/temp") && save)
+                AssetDatabase.CreateFolder("Assets", "temp");
+#endif
+            foreach (var renderer in renderers)
+            {
+                if (renderer.gameObject.CompareTag("EditorOnly"))
+                    continue;
+                indices.Clear();
+                usedIndices.Clear();
+                orderIndices.Clear();
+                bindposes.Clear();
+                newBindposes.Clear();
+                newTransforms.Clear();
+                var mesh = Instantiate(renderer.sharedMesh);
+                var weights1 = mesh.GetAllBoneWeights();
+                var weights = new NativeArray<BoneWeight1>(weights1, Allocator.Temp);
+                //var weights = mesh.boneWeights;
+                var bonesPerVertex = mesh.GetBonesPerVertex();
+                foreach (var item in weights)
+                {
+                    usedIndices.Add(item.boneIndex);
+                }
+                /*foreach (var item in weights)
+                {
+                    usedIndices.Add(item.boneIndex0);
+                    usedIndices.Add(item.boneIndex1);
+                    usedIndices.Add(item.boneIndex2);
+                    usedIndices.Add(item.boneIndex3);
+                }*/
+                var bones = renderer.bones;
+                orderIndices.AddRange(usedIndices);
+                var ordered = orderIndices.OrderBy(a => a).ToArray();
+                int index = 0;
+                mesh.GetBindposes(bindposes);
+                foreach (var boneIndex in ordered)
+                {
+                    indices.Add(boneIndex, index);
+                    newBindposes.Add(bindposes[boneIndex]);
+                    newTransforms.Add(bones[boneIndex]);
+                    index++;
+                }
+                if (index == bones.Length)
+                    continue;
+
+                for (int i = 0; i < weights.Length; i++)
+                {
+                    var item = weights[i];
+                    item.boneIndex = indices[item.boneIndex];
+                    weights[i] = item;
+                }
+
+                 mesh.SetBoneWeights(bonesPerVertex, weights);
+                 weights.Dispose();
+                 
+                /*for (int i = 0; i < weights.Length; i++)
+                {
+                    BoneWeight item = weights[i];
+                    item.boneIndex0 = indices[item.boneIndex0];
+                    item.boneIndex1 = indices[item.boneIndex1];
+                    item.boneIndex2 = indices[item.boneIndex2];
+                    item.boneIndex3 = indices[item.boneIndex3];
+                    weights[i] = item;
+                }
+                mesh.boneWeights = weights;*/
+                mesh.bindposes = newBindposes.ToArray();
+                renderer.bones = newTransforms.ToArray();
+                sw1.Stop();
+                sw2.Start();
+
+#if UNITY_EDITOR
+                if (save)
+                {
+                    var name = $"Assets/temp/RemoveUnneededBonesTempAsset-{root.name}-{renderer.name}-{mesh.name}-{Guid.NewGuid()}.mesh";
+                    AssetDatabase.CreateAsset(mesh, name);
+                    createdAssetNames.Add(name);
+                }
+#endif
+                sw2.Stop();
+                sw1.Start();
+                renderer.sharedMesh = mesh;
+            }
+            sw1.Stop ();
+            Debug.Log($"cleanup took {sw1.Elapsed.TotalMilliseconds:N2} ms, saving took {sw2.Elapsed.TotalMilliseconds:N2} ms. ");
+
+#if UNITY_EDITOR
+            if (save)
+            {
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+#endif
+        }
+    }
+}
